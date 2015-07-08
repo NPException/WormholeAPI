6e7160a87091390d1fbefe3341fbdedcffe0db8c
NPException <npexception42@gmail.com>
2015-07-08 08:49:15 +0200

    added implementation suggestion to javadoc

--- src/main/java/de/cdmp/api/wormhole/IWormhole.java
+++ src/main/java/de/cdmp/api/wormhole/IWormhole.java
@@ -1,5 +1,6 @@
 package de.cdmp.api.wormhole;
 
+import java.util.Collections;
 import java.util.List;
 
 import net.minecraft.block.Block;
@@ -9,7 +10,16 @@ import net.minecraftforge.common.util.ForgeDirection;
 public interface IWormhole {
 
 	/**
-	 * Gets a list of all available adjacent blocks.
+	 * Gets a list of all available adjacent blocks.<br>
+	 * <br>
+	 * <i>It is recomended for implemtations of this interface to keep a List and a view to that list
+	 * (using {@link Collections#unmodifiableList(java.util.List)}) for each {@link ForgeDirection} internally.
+	 * That way only the backing list needs to be modified and the unmodifiable view can be returned.
+	 * This will prevent unnecessary GC overhead due to Object creation.</i><br>
+	 * <br>
+	 * <b>Attention: </b> Though this kind of implementation is recomended, it is not guaranteed.
+	 * So you should never expect a List you got from calling this method to be the current representation of the wormholes targets.
+	 * (Because the target list might only be updated due to a call to this method, for example.)
 	 *
 	 * @param fromDirection
 	 *           The direction the request is coming from
